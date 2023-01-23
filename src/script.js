d42be60b2c72d7f2ba9106470500651d85b53fb5
Benjamin Bowles <benjamin.d.bowles@gmail.com>
2023-01-23 15:23:22 -0600

    styling

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
-let menu = document.getElementsByClassName('menu')[0];
+let menu = document.getElementById("menu_container");
+let menu_buttons = document.getElementById("menu_buttons");
 let menu_toggle = document.getElementsByClassName('menu_toggle')[0];
 let menu_opened = false;
 
@@ -7,4 +8,5 @@ function toggleMenu() {
 	menu_opened = !menu_opened;
 	menu.classList.toggle('opened', menu_opened);
 	menu_toggle.classList.toggle('opened', menu_opened);
+	menu_buttons.classList.toggle('opened', menu_opened);
 }
