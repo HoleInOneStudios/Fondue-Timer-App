3876a9f46c31980771b336b63e31751ef8371c88
Benjamin Bowles <benjamin.d.bowles@gmail.com>
2023-01-23 19:10:51 -0600

    yes

--- src/script.js
+++ src/script.js
@@ -1,8 +1,8 @@
+// Sidebar Menu
 let menu = document.getElementById("menu_container");
 let menu_buttons = document.getElementById("menu_buttons");
 let menu_toggle = document.getElementsByClassName('menu_toggle')[0];
 let menu_opened = false;
-
 
 function toggleMenu() {
 	menu_opened = !menu_opened;
@@ -11,14 +11,7 @@ function toggleMenu() {
 	menu_buttons.classList.toggle('opened', menu_opened);
 }
 
-let fork_list = document.getElementById('fork_list');
-let forks = []
-
-let food_list = document.getElementById('food_list');
-let foods = []
-
-let timer_list = document.getElementById('timer_list');
-let timers = []
+// Main timer logic
 
 class Fork {
 	constructor(name, color) {
@@ -36,7 +29,7 @@ class Fork {
 }
 
 class Food {
-	constructor(name, color, time) {
+	constructor(name, icon, time) {
 		this.name = name;
 		this.icon = icon;
 		this.time = time;
@@ -49,4 +42,33 @@ class Food {
 					<span class="name">${this.name}</span>
 				</div>`
 	}
+}
+
+var fork_list = document.getElementById('fork_list');
+var forks = [];
+
+forks.push(new Fork('Fork 1', 'Red'));
+forks.push(new Fork('Fork 2', 'Blue'));
+forks.push(new Fork('Fork 3', 'Green'));
+
+var food_list = document.getElementById('food_list');
+var foods = [];
+
+foods.push(new Food('Food 1', 'H', 10));
+foods.push(new Food('Food 2', 'P', 15));
+foods.push(new Food('Food 3', 'F', 20));
+
+var timer_list = document.getElementById('timer_list');
+var timers = [];
+
+function redrawOptions() {
+	fork_list.innerHTML = '';
+	forks.forEach(fork => {
+		fork_list.innerHTML += fork.element;
+	});
+
+	food_list.innerHTML = '';
+	foods.forEach(food => {
+		food_list.innerHTML += food.element;
+	});
 }
